7b3ac489d623a31d9b842bbe244f086a903f3102
OSTASHOLEH <66119430+OSTASHOLEH@users.noreply.github.com>
2024-03-22 18:28:47 +0200

    Pages
    added new functions LoadPage and LoadData that use local storage, added basic search mechanics

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 function FindAtDB(){
-    window.location.reload();
+    var searchValue = document.getElementById("search_input").value;
+    LoadPage(searchValue);
+}
+function LoadPage(name) {
+    localStorage.setItem("imageName", name);
+    window.location.href = "template.html";
+}
+function LoadData() {
+    let imageName = localStorage.getItem("imageName");
+    if (imageName) {
+        var main_image = document.getElementById("Main_IC_Photo");
+        var img_name = document.getElementById("Img_Name");
+        main_image.src = "images/" + imageName + ".jpg";
+        main_image.alt = imageName;
+        img_name.innerHTML = imageName.toUpperCase();
+    }
+    else{
+        console.error("not loaded");
+    }
 }
