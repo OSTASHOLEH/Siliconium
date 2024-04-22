e80937400ae35ed03fc5b6af2c89fefb93d5a423
OSTASHOLEH <66119430+OSTASHOLEH@users.noreply.github.com>
2024-04-22 23:15:33 +0300

    core

--- script.js
+++ script.js
@@ -11,9 +11,13 @@ function LoadData() {
     if (imageName) {
         var main_image = document.getElementById("Main_IC_Photo");
         var img_name = document.getElementById("Img_Name");
-        main_image.src = "images/" + imageName + ".jpg";
+        var discription = document.getElementById("frames");
+        var dtsheet =document.getElementById("link_d");
+        main_image.src = "datasheets/" + imageName + "/"+ imageName + ".png";
         main_image.alt = imageName;
+        discription.src = "datasheets/" + imageName + "/"+ imageName + ".txt";
         img_name.innerHTML = imageName.toUpperCase();
+        dtsheet.setAttribute("href","datasheets/" + imageName + "/"+ imageName.toUpperCase() + ".PDF");
     }
     else{
         console.error("not loaded");
