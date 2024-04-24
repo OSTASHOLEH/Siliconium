c983b369a5370932c23d7bc391bd3cf7d3fd3682
OSTASHOLEH <66119430+OSTASHOLEH@users.noreply.github.com>
2024-04-24 16:13:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,6 +18,10 @@ function LoadData() {
         discription.src = "datasheets/" + imageName.toLowerCase() + "/"+ imageName.toLowerCase() + ".txt";
         img_name.innerHTML = imageName.toUpperCase();
         dtsheet.setAttribute("href","datasheets/" + imageName.toLowerCase() + "/"+ imageName.toUpperCase() + ".PDF");
+        if(!"datasheets/" + imageName.toLowerCase() + "/"+ imageName.toUpperCase() + ".PDF"){
+            console.error("wrong");
+            window.alert("Error occuped. Try to use other name of IC");
+        }
     }
     else{
         window.location.href="index.html";
